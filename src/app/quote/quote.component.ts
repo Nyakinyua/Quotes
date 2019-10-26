5a017962af14fa7f5ae5372e9836e32221d6ed18
Nyakinyua <wanyakinyua968@gmail.com>
2019-10-26 17:15:43 +0300

    add button to toggle quote details

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,6 +10,10 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
     new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
   ]
+
+  toggleDetails(index){
+    this.quote[index].showDescription = !this.quote[index].showDescription;
+  }
   constructor() { }
 
   ngOnInit() {
