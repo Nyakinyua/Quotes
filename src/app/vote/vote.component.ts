f5e3018c23f8d6e3b8cf1b9bbf46e4a540b29ea3
Nyakinyua <wanyakinyua968@gmail.com>
2019-10-27 08:25:15 +0300

    add ionicons to styles.css

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -13,11 +13,11 @@ export class VoteComponent implements OnInit {
   @Output() isUpVoted = new EventEmitter<boolean>();
   @Output() isDownVoted = new EventEmitter<boolean>();
 
-  quoteUpVote(upvote: boolean) {
+  quoteupVote(upvote: boolean) {
     this.isUpVoted.emit(upvote);
   }
 
-  quoteDownVote(downvote: boolean) {
+  quotedownVote(downvote: boolean) {
     this.isDownVoted.emit(downvote);
   }
   constructor() { }
