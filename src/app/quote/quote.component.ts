49c6a004eb2e7c9678dd839455215fdb80173586
Nyakinyua <wanyakinyua968@gmail.com>
2019-10-27 22:46:32 +0300

    add card-text to quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,8 +8,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quote:Quote[] = [
     new Quote(1, 'All you need is love. But a little chocolate now and then does not hurt', 'Charles M. Schulz','new','one',0,0 ,new Date(2019,10,12)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot','me', 'you',0,0,new Date (2017,3,14)),
-    new Quote(3,"I like the night", "Without the dark, we'd never see the stars",'me','you',0,0, new Date(2019,10,25)),
+    new Quote(2,'Lord, grant that I might not so much seek to be loved as to love','Francis of Assisi','me', 'you',0,0,new Date (2017,3,14)),
+    new Quote(3,"I like the night Without the dark, we'd never see the stars" ,'Ritu Ghatourey','Rose','stomp',0,0, new Date(2019,10,25)),
+    new Quote(4,'From now on, ending a sentence with a preposition is something up with which I will not put','Winston Churchill','Sheila','Alicia',0,0, new Date(2019,12,10) )
   ];
 
    
